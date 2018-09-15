1fe47fb0a7b6becab30bc16ef43f82d86bb599c4
Jai-K0819 <jaikandpal88@gmail.com>
2018-09-15 10:22:52 +0530

    Two changes committed:
    1. Updated ReverseString.java in StringsCodes package
    2. Added PatternsTwo.java file in patternscodes1 package

--- JAVACodes/src/StringCodes/ReverseStrings.java
+++ JAVACodes/src/StringCodes/ReverseStrings.java
@@ -4,6 +4,14 @@ public class ReverseStrings {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String s = "i love my country";
+		String[] splited = s.split("\\s+");
+		int len = splited.length;
+		
+		for(int i=len-1; i>=0; i--){
+			splited[i] = splited[i].toUpperCase();
+			System.out.print(splited[i]+ " ");
+		}
 
 	}
 
